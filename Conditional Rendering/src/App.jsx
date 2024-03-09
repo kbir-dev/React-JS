4d3755698d674bf86e4d4d6be57160b92089d65b
kbir079 <ks400068@gmail.com>
2024-03-09 20:07:20 +0530

    added use

---
+++ Conditional Rendering/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [showbtn, setshowbtn] = useState(false);
+  const [todos, setTodos] = useState([
+    {
+      title: "Hello Bois1",
+      desc: "Ok we are desc1",
+    },
+    {
+      title: "Hello Bois2",
+      desc: "Ok we are desc2",
+    },
+    {
+      title: "Hello Bois3",
+      desc: "Ok we are desc3",
+    },
+  ]);
+
+  const Todo = ({todo}) => {
+    return (
+      <div className="m-4 border border-1 border-purple-400">
+        <div className="todo">{todo.title}</div>
+        <div className="todo">{todo.desc}</div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      {/* {showbtn?<button >I am true</button> :<button >I am false</button>} */}
+      {/* {showbtn && <button >I am here</button>} */}
+      {todos.map(todo => {
+        return <div key={todo.title} className="m-4 border border-1 border-purple-400">
+
+          <div className="todo">{todo.title}</div>
+          <div className="todo">{todo.desc}</div>
+        </div>
+      })}
+      <div className="card">
+        <button onClick={() => setshowbtn(!showbtn)}>Toggle Button</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
